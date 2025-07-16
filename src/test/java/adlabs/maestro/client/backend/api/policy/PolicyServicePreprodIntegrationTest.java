7d61c30aef19b72f7ecf7b361479808934f4401e
Derrick Kwagala <gratefulderrick@gmail.com>
2025-07-16 19:18:38 +0300

    Add Policy integration tests

---
+++ src/test/java/adlabs/maestro/client/backend/api/policy/PolicyServicePreprodIntegrationTest.java
@@ -0,0 +1,93 @@
+package adlabs.maestro.client.backend.api.policy;
+
+import adlabs.maestro.client.backend.api.asset.model.PaginatedAssetInfoConcise;
+import adlabs.maestro.client.backend.api.base.Result;
+import adlabs.maestro.client.backend.api.base.exception.ApiException;
+import adlabs.maestro.client.backend.factory.BackendFactory;
+import adlabs.maestro.client.backend.factory.options.Options;
+import adlabs.maestro.client.backend.api.policy.model.PaginatedPolicyHolder;
+import adlabs.maestro.client.backend.api.policy.model.PaginatedPolicyHolderAccount;
+import adlabs.maestro.client.backend.api.policy.model.PaginatedPolicyMintTransaction;
+import adlabs.maestro.client.backend.api.policy.model.PaginatedPolicyTransaction;
+import adlabs.maestro.client.backend.api.policy.model.PaginatedPolicyUtxo;
+import adlabs.maestro.client.backend.api.policy.model.TimestampedPolicyInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PolicyServicePreprodIntegrationTest {
+
+    private PolicyService policyService;
+
+    @BeforeAll
+    public void setup() {
+        policyService = BackendFactory.getMaestroPreprodService().getPolicyService();
+    }
+
+    @Test
+    void getPolicyAccountsTest() throws ApiException {
+        var policyID = "2c5fdca5caed1095d077cb23b49ccb170c3be4245915ec169c5c57a5";
+        Result<PaginatedPolicyHolderAccount> result = policyService.getPolicyAccounts(policyID, Options.EMPTY);
+        Assertions.assertTrue(result.isSuccessful());
+        Assertions.assertNotNull(result.getValue());
+        log.info(result.getValue().toString());
+    }
+
+    @Test
+    void getPolicyAddressesTest() throws ApiException {
+        var policyID = "2c5fdca5caed1095d077cb23b49ccb170c3be4245915ec169c5c57a5";
+        Result<PaginatedPolicyHolder> result = policyService.getPolicyAddresses(policyID, Options.EMPTY);
+        Assertions.assertTrue(result.isSuccessful());
+        Assertions.assertNotNull(result.getValue());
+        log.info(result.getValue().toString());
+    }
+
+    @Test
+    void getPolicyAssetsTest() throws ApiException {
+        var policyID = "2c5fdca5caed1095d077cb23b49ccb170c3be4245915ec169c5c57a5";
+        Result<PaginatedAssetInfoConcise> result = policyService.getPolicyAssets(policyID, Options.EMPTY);
+        Assertions.assertTrue(result.isSuccessful());
+        Assertions.assertNotNull(result.getValue());
+        log.info(result.getValue().toString());
+    }
+
+    @Test
+    void getPolicyInfoTest() throws ApiException {
+        var policyID = "2c5fdca5caed1095d077cb23b49ccb170c3be4245915ec169c5c57a5";
+        Result<TimestampedPolicyInfo> result = policyService.getPolicyInfo(policyID);
+        Assertions.assertTrue(result.isSuccessful());
+        Assertions.assertNotNull(result.getValue());
+        log.info(result.getValue().toString());
+    }
+
+    @Test
+    void getPolicyMintsTest() throws ApiException {
+        var policyID = "2c5fdca5caed1095d077cb23b49ccb170c3be4245915ec169c5c57a5";
+        Result<PaginatedPolicyMintTransaction> result = policyService.getPolicyMints(policyID, Options.EMPTY);
+        Assertions.assertTrue(result.isSuccessful());
+        Assertions.assertNotNull(result.getValue());
+        log.info(result.getValue().toString());
+    }
+
+    @Test
+    void getPolicyTxsTest() throws ApiException {
+        var policyID = "2c5fdca5caed1095d077cb23b49ccb170c3be4245915ec169c5c57a5";
+        Result<PaginatedPolicyTransaction> result = policyService.getPolicyTxs(policyID, Options.EMPTY);
+        Assertions.assertTrue(result.isSuccessful());
+        Assertions.assertNotNull(result.getValue());
+        log.info(result.getValue().toString());
+    }
+
+    @Test
+    void getPolicyUtxosTest() throws ApiException {
+        var policyID = "2c5fdca5caed1095d077cb23b49ccb170c3be4245915ec169c5c57a5";
+        Result<PaginatedPolicyUtxo> result = policyService.getPolicyUtxos(policyID, Options.EMPTY);
+        Assertions.assertTrue(result.isSuccessful());
+        Assertions.assertNotNull(result.getValue());
+        log.info(result.getValue().toString());
+    }
+}
