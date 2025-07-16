5bbcb5fc5f4be6b282d865ad15d8d706a7f853ae
Derrick Kwagala <gratefulderrick@gmail.com>
2025-07-16 19:10:12 +0300

    Add the addressAPI client feature

--- src/main/java/adlabs/maestro/client/backend/api/address/model/PaginatedUtxoWithSlot.java
+++ src/main/java/adlabs/maestro/client/backend/api/address/model/PaginatedUtxoWithSlot.java
@@ -1,4 +1,4 @@
-package adlabs.maestro.client.backend.api.address;
+package adlabs.maestro.client.backend.api.address.model;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
--- src/main/java/adlabs/maestro/client/backend/api/address/model/TimestampedBalance.java
+++ src/main/java/adlabs/maestro/client/backend/api/address/model/TimestampedBalance.java
@@ -1,11 +1,11 @@
-package adlabs.maestro.client.backend.models;
+package adlabs.maestro.client.backend.api.address.model;
 
+import adlabs.maestro.client.backend.models.LastUpdated;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
-import adlabs.maestro.client.backend.api.address.model.Balance;
 
 /**
  * Timestamped response. Returns the endpoint response data along with the chain-tip of the indexer, which details at which point in the chain's history the data was correct as-of.
