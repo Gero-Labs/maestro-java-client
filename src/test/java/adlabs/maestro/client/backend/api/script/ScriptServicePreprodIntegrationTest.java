d28d9d64a9e3840465a3219510facf3145c133ef
Derrick Kwagala <gratefulderrick@gmail.com>
2025-07-22 17:35:24 +0300

    Add the script tests

---
+++ src/test/java/adlabs/maestro/client/backend/api/script/ScriptServicePreprodIntegrationTest.java
@@ -0,0 +1,33 @@
+package adlabs.maestro.client.backend.api.script;
+
+import adlabs.maestro.client.backend.api.base.Result;
+import adlabs.maestro.client.backend.api.base.exception.ApiException;
+import adlabs.maestro.client.backend.api.script.model.TimestampedScriptFirstSeen;
+import adlabs.maestro.client.backend.factory.BackendFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ScriptServicePreprodIntegrationTest {
+
+    private ScriptsService scriptsService;
+
+    @BeforeAll
+    public void setup() {
+        scriptsService = BackendFactory.getMaestroPreprodService().getScriptsService();
+    }
+
+    @Test
+    void getScriptInformationTest() throws ApiException {
+        String scriptHash = "a8e9f8f34fd631b1d5b9f41a90f4abc0d3935cea7baba0bb34c96f59";
+        Result<TimestampedScriptFirstSeen> scriptInformationResult = scriptsService.getScriptByHash(scriptHash);
+        Assertions.assertTrue(scriptInformationResult.isSuccessful());
+        Assertions.assertNotNull(scriptInformationResult.getValue());
+        log.info(scriptInformationResult.getValue().toString());
+    }
+
+}
