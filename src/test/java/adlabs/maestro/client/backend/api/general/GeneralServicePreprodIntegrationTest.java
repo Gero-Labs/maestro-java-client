2c27a48e7e97d360f2ed1e2d961557c583dd8cb8
Derrick Kwagala <gratefulderrick@gmail.com>
2025-07-22 17:35:14 +0300

    Add the general tests

---
+++ src/test/java/adlabs/maestro/client/backend/api/general/GeneralServicePreprodIntegrationTest.java
@@ -0,0 +1,61 @@
+package adlabs.maestro.client.backend.api.general;
+
+import adlabs.maestro.client.backend.api.base.Result;
+import adlabs.maestro.client.backend.api.base.exception.ApiException;
+import adlabs.maestro.client.backend.api.general.model.TimestampedChainTip;
+import adlabs.maestro.client.backend.api.general.model.TimestampedEraSummaries;
+import adlabs.maestro.client.backend.api.general.model.TimestampedProtocolParameters;
+import adlabs.maestro.client.backend.api.general.model.TimestampedSystemStart;
+import adlabs.maestro.client.backend.factory.BackendFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GeneralServicePreprodIntegrationTest {
+
+    private GeneralService generalService;
+
+    @BeforeAll
+    public void setup() {
+        generalService = BackendFactory.getMaestroPreprodService().getGeneralService();
+    }
+
+
+    @Test
+    void getChainTipTest() throws ApiException {
+        Result<TimestampedChainTip> chainTipResult = generalService.getChainTip();
+        assertTrue(chainTipResult.isSuccessful());
+        assertNotNull(chainTipResult.getValue());
+        log.info(chainTipResult.getValue().toString());
+    }
+
+    @Test
+    void getEraSummariesTest() throws ApiException {
+        Result<TimestampedEraSummaries> eraSummariesResult = generalService.getEraSummaries();
+        assertTrue(eraSummariesResult.isSuccessful());
+        assertNotNull(eraSummariesResult.getValue());
+        log.info(eraSummariesResult.getValue().toString());
+    }
+
+    @Test
+    void getProtocolParametersTest() throws ApiException {
+        Result<TimestampedProtocolParameters> protocolParametersResult = generalService.getProtocolParameters();
+        assertTrue(protocolParametersResult.isSuccessful());
+        assertNotNull(protocolParametersResult.getValue());
+        log.info(protocolParametersResult.getValue().toString());
+    }
+
+    @Test
+    void getSystemStartTest() throws ApiException {
+        Result<TimestampedSystemStart> systemStartResult = generalService.getSystemStart();
+        assertTrue(systemStartResult.isSuccessful());
+        assertNotNull(systemStartResult.getValue());
+        log.info(systemStartResult.getValue().toString());
+    }
+
+}
